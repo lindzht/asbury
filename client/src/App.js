e5f7cc16bb249ac5679d761aaf259a214e8a737c
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-07-02 15:51:19 -0400

    installed react-router, react-responsive, and images from backup

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Header from './components/Header';
 import LandingPage from './components/Landing';
 import Footer from './components/Footer';
 import About from './components/About';
-import UpcomingShows from './components/UpcomingShows';
+import UpcomingShows from './components/UpcShows';
 import PastShows from './components/PastShows';
 import BookUs from './components/BookUs';
 import Sponsors from './components/Sponsors';
