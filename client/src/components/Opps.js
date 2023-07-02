e0d32ce53f80926b03db8b5445ef709848191bb6
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-07-02 15:45:19 -0400

    copied over opportunity, past show card, and past shows page

---
+++ client/src/components/Opps.js
@@ -0,0 +1,10 @@
+
+function OppPage () {
+    return(
+        <div id="opp-container">
+            <h1>Opportunity Page</h1>
+        </div>
+    )
+}
+
+export default OppPage;
---
+++ client/src/components/PastShowCard.js
@@ -0,0 +1,9 @@
+function PastCard () {
+    return(
+        <div id="pcard-container">
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default PastCard;
