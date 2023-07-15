2e181b82758f7d2db2611efcb2a0dfe5de95c998
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-07-15 12:10:21 -0400

    installed react-reveal. applied to tcards on landing

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -1,27 +1,34 @@
 import TestimonialCard from "./TestimonialCard";
+import Fade from 'react-reveal/Fade';
 
 function Testimonials() {
     return(
         <div id="testimonials-container">
+            <Fade bottom>
+                <TestimonialCard 
+                    name={"Jason Reitman"} 
+                    title={"Academy Award nominated Director, Asbury Shorts Concert Guest Host"} 
+                    credits={`"JUNO", "Young Adult", "Up in the Air", "Thank You for Smoking"`}
+                    quote={"The Best Short Film Show I’ve Ever Seen"}
+                    img={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"} />
+            </Fade>
+            <Fade bottom>
+                <TestimonialCard
+                    id="r-card"
+                    name={"Jason Reitman"} 
+                    title={"Academy Award nominated Director, Asbury Shorts Concert Guest Host"} 
+                    credits={`"JUNO", "Young Adult", "Up in the Air", "Thank You for Smoking"`}
+                    quote={"The Best Short Film Show I’ve Ever Seen"}
+                    img={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"} />
+            </Fade>
+            <Fade bottom>
             <TestimonialCard 
                 name={"Jason Reitman"} 
                 title={"Academy Award nominated Director, Asbury Shorts Concert Guest Host"} 
                 credits={`"JUNO", "Young Adult", "Up in the Air", "Thank You for Smoking"`}
                 quote={"The Best Short Film Show I’ve Ever Seen"}
                 img={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"} />
-            <TestimonialCard
-                id={"r-card" }
-                name={"Jason Reitman"} 
-                title={"Academy Award nominated Director, Asbury Shorts Concert Guest Host"} 
-                credits={`"JUNO", "Young Adult", "Up in the Air", "Thank You for Smoking"`}
-                quote={"The Best Short Film Show I’ve Ever Seen"}
-                img={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"} />
-            <TestimonialCard 
-                name={"Jason Reitman"} 
-                title={"Academy Award nominated Director, Asbury Shorts Concert Guest Host"} 
-                credits={`"JUNO", "Young Adult", "Up in the Air", "Thank You for Smoking"`}
-                quote={"The Best Short Film Show I’ve Ever Seen"}
-                img={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"} />
+            </Fade>
         </div>
     )
 }
