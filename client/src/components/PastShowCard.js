c5aeaa4e06cbbe9435dd1285d471f06a9cd91857
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-07-27 18:37:31 -0400

    styled CSS for past shows card

--- client/src/components/PastShowCard.js
+++ client/src/components/PastShowCard.js
@@ -1,9 +1,20 @@
-function PastCard () {
+function PastShowCard ( {img, title, date, location, host, time, cost, desc, url} ) {
     return(
-        <div id="pcard-container">
-            <h1>About</h1>
+        <div className="past-show-card-container">
+            <div className="past-show-c-header">
+                <img src={img} alt={title} />
+                <div id="past-s-c-title">
+                    <h2>{date}</h2>
+                    <h1>{title}</h1>
+                </div>
+            </div>
+            <div className="past-show-c-desc">
+                {host ? <h3>Guest Host: {host}</h3> : null}
+                <h4>{location}</h4>
+                <p>{desc}</p>
+            </div>
         </div>
     )
 }
 
-export default PastCard;
+export default PastShowCard;
