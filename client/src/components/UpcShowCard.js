0ed24b4c65b8151734226b7416974e4d4d59b1f6
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-07-27 17:58:06 -0400

    updated CSS style for mobile upcoming show card

--- client/src/components/UpcShowCard.js
+++ client/src/components/UpcShowCard.js
@@ -5,14 +5,15 @@ function UpcomingShowCard ( {img, title, date, location, host, time, cost, desc,
         <div className="up-show-card-container">
             <div className="up-show-c-header">
                 <img src={img} alt={title} />
-                <h1>{title}</h1>
+                <div id="up-s-c-title">
+                    <h2>{date}</h2>
+                    <h1>{title}</h1>
+                </div>
             </div>
             <div className="up-show-c-desc">
                 {host ? <h3>Guest Host: {host}</h3> : null}
-                <h4>{date}</h4>
-                <h4>{time}</h4>
-                <h4>{cost}</h4>
-                <h4><em>{location}</em></h4>
+                <h4>{time} | {cost}</h4>
+                <h4>{location}</h4>
                 <p>{desc}</p>
             </div>
             <a href={`${url}`} className="button" alt="Tickets" target="_blank" rel="noreferrer">Tickets</a>
