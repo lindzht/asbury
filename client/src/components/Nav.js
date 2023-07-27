2aa5547ff4aa92f3c70742519a2ec583d6e1ed57
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-07-27 19:07:05 -0400

    constructing desktop nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,45 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 
 function Nav () {
+
+    const [displayShowSub, setDisplayShowSub] = useState(false);
+    const [displayOppSub, setDisplayOppSub] = useState(false);
+    
+    const handleDisplayShow = () => {
+        setDisplayShowSub(!displayShowSub);
+    }
+    const handleDisplayOpp = () => {
+        setDisplayOppSub(!displayOppSub);
+    }
+
+
     return(
         <div id="nav-container">
-            <h1>Nav</h1>
+            <Link to="/about">
+                <h1>About</h1>
+            </Link>
+    
+            <h1 onClick={handleDisplayShow}>Shows</h1>
+                {displayShowSub ? 
+                <div id="nav-show-sub">
+                    <Link to="upcoming" >Upcoming Shows</Link>
+                    <Link to="past">Past Shows</Link>  
+                </div> : null}
+            
+            <h1 onClick={handleDisplayOpp}>Opportunities</h1>
+                {displayOppSub ? 
+                <div id="nav-op-sub">
+                    <Link to="booking" >Book Us</Link>
+                    <Link to="sponsorship" >Sponsorship</Link>
+                    <Link to="submissions" >Submissions</Link>
+                </div> : null}
+            
+            <Link to="/about">
+                <h1>Contact</h1>
+            </Link>
+            
         </div>
     )
 }
