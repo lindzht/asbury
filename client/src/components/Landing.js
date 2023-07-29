c7768d9d18fd5a8181ad24ded4e6eda40d30c8f3
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-07-29 16:03:35 -0400

    added css for landing card & landing about for desktop

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -17,16 +17,46 @@ function LandingPage ({isDesktop}) {
         <div id="landing-container">
             {isDesktop ? 
             <div id="l-hero-s"> 
-                <LandingCard img="https://www.syfy.com/sites/syfy/files/styles/amp_featured_image/public/2019/10/faceoff_cage.jpg?h=ba463b8a" />
-            </div> :  
+                <LandingCard 
+                    img="https://www.syfy.com/sites/syfy/files/styles/amp_featured_image/public/2019/10/faceoff_cage.jpg?h=ba463b8a" 
+                    title="CUNY Graduate Theater"
+                    date="Aug 18"
+                    location="365 Fifth Ave"
+                    host="Ken Dashow"    
+                    />
+                <LandingCard 
+                    img="https://www.syfy.com/sites/syfy/files/styles/amp_featured_image/public/2019/10/faceoff_cage.jpg?h=ba463b8a" 
+                    title="CUNY Graduate Theater"
+                    date="Aug 18"
+                    location="365 Fifth Ave"
+                    host="Ken Dashow"    
+                    />
+                <LandingCard 
+                    img="https://www.syfy.com/sites/syfy/files/styles/amp_featured_image/public/2019/10/faceoff_cage.jpg?h=ba463b8a" 
+                    title="CUNY Graduate Theater"
+                    date="Aug 18"
+                    location="365 Fifth Ave"
+                    host="Ken Dashow"    
+                    />
+            </div> 
+            :  
             <div id="l-hero-s"> 
-                <LandingSlideMob images={slideshow1} time={time}/>
+                <LandingSlideMob 
+                    images={slideshow1} 
+                    time={time}/>
             </div>}
             
             <div id="landing-about">
                 <h2>Asbury Shorts USA</h2>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949" alt="placeholder" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                {isDesktop? 
+                <div id="land-img-row">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949" alt="placeholder" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949" alt="placeholder" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949" alt="placeholder" />
+                </div>   
+                : <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949" alt="placeholder" />}
             </div>
 
             <Testimonials />
--- client/src/components/LandingCard.js
+++ client/src/components/LandingCard.js
@@ -4,17 +4,15 @@ import { Link } from "react-router-dom";
 function LandingCard ({img, title, date, location, host, time, cost, desc, url}) {
     return(
         <div className="landcard-container">
-            <div className="landcard-header">
-                <img src={img} alt={title} />
-                <div id="landcard-title">
-                    <h1>{title}</h1>
-                    <h2>{date}</h2>
-                </div>
-            </div>
-            <div className="landcard-desc">
-                {host ? <h3>Guest Host: {host}</h3> : null}
+            <div className="landcard-content" style={{backgroundImage: `url(${img})`}}>
+                {/* <img src={img} alt={title} /> */}
                 <Link to="/upcoming">
-                    <h3>More Information</h3>
+                    <div id="landcard-title">
+                        <h2>{date}</h2>
+                        <h1>{title}</h1>
+                        <h3>{location}</h3>
+                        {host ? <h3>Guest Host: {host}</h3> : null}                                  
+                    </div>
                 </Link>
             </div>
         </div>
