75af74a46d876987cf73788a704fb86df6a1d445
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-08-05 10:47:28 -0400

    started working on login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,6 +4,14 @@ function Login () {
     return(
         <div id="login-container">
             <h1>Login Page</h1>
+
+            <div id="login-form">
+                <form>
+                    <label for="" ></label>
+                    <input type="text" id="email" name="email" placeholder="Email"/>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         </div>
     )
 }
