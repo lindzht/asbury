244d1f7fa636b12d30e7ac04c85d6cc6e5accc27
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-08-05 13:26:29 -0400

    added desktop styles to past shows card and past shows page

--- client/src/components/PastShows.js
+++ client/src/components/PastShows.js
@@ -1,9 +1,13 @@
 import PastShowCard from "./PastShowCard";
+import Banner from "./Banner";
+import poster from "../images/theater_light.jpg";
+
 
 function PastShows() {
     return(
         <div id="past-container">
-            <h1>Past Shows</h1>
+            <Banner title="Past Shows" img={poster} />
+
             <PastShowCard 
                 img="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"
                 title="CUNY Graduate Theater"
@@ -12,7 +16,52 @@ function PastShows() {
                 location="365 Fifth Ave (B/W 34th & 35th Street"
                 cost="$25"
                 host="Ken Dashow"
-                desc="tkt. tktk . tkt tktktktkt .t ktktktktktkt."/>
+                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+            <PastShowCard 
+                img="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"
+                title="CUNY Graduate Theater"
+                date="Aug 18"
+                time="7:00PM"
+                location="365 Fifth Ave (B/W 34th & 35th Street"
+                cost="$25"
+                host="Ken Dashow"
+                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+            <PastShowCard 
+                img="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"
+                title="CUNY Graduate Theater"
+                date="Aug 18"
+                time="7:00PM"
+                location="365 Fifth Ave (B/W 34th & 35th Street"
+                cost="$25"
+                host="Ken Dashow"
+                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+            <PastShowCard 
+                img="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"
+                title="CUNY Graduate Theater"
+                date="Aug 18"
+                time="7:00PM"
+                location="365 Fifth Ave (B/W 34th & 35th Street"
+                cost="$25"
+                host="Ken Dashow"
+                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+            <PastShowCard 
+                img="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"
+                title="CUNY Graduate Theater"
+                date="Aug 18"
+                time="7:00PM"
+                location="365 Fifth Ave (B/W 34th & 35th Street"
+                cost="$25"
+                host="Ken Dashow"
+                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+            <PastShowCard 
+                img="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949"
+                title="CUNY Graduate Theater"
+                date="Aug 18"
+                time="7:00PM"
+                location="365 Fifth Ave (B/W 34th & 35th Street"
+                cost="$25"
+                host="Ken Dashow"
+                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
         </div>
     )
 }
