a8398e3379c29d4b81074f559756020fd2ff21c2
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-08-06 15:27:49 -0400

    added form to login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 
 
-function Login () {
+function Login ({user}) {
+
+if (user) {
     return(
         <div id="login-container">
             <h1>Login Page</h1>
@@ -15,5 +17,88 @@ function Login () {
         </div>
     )
 }
+else {
+    return(
+        <div id="login-container">
+            <h1>You're Logged In!</h1>
+
+            <div id="new-show-container">
+                <form id="new-show-form">
+                    <label>Banner Image</label>
+                    <input 
+                        type="text" 
+                        id="img" 
+                        name="img"
+                        placeholder="Image" />
+                    <label>Venue</label>
+                    <input 
+                        type="text" 
+                        id="title" 
+                        name="title"
+                        placeholder="Title" />
+                    <label>Show Date</label>
+                    <input 
+                        type="date" 
+                        id="date" 
+                        name="date"
+                        placeholder="Aug 18" />
+                    <label>Show Time</label>
+                    <input 
+                        type="text" 
+                        id="time" 
+                        name="time"
+                        placeholder="7:00 PM" />
+                    <label>Address</label>
+                    <input 
+                        type="text" 
+                        id="location" 
+                        name="location"
+                        placeholder="365 Fifth Ave" />
+                    <label>Ticket Cost</label>
+                    <input 
+                        type="text" 
+                        id="cost" 
+                        name="cost"
+                        placeholder="$25" />
+                    <label>Host</label>
+                    <input 
+                        type="text" 
+                        id="host" 
+                        name="host"
+                        placeholder="First Name Last Name" />
+                    <label>Ticket URL</label>
+                    <input 
+                        type="text" 
+                        id="url" 
+                        name="url"
+                        placeholder="URL" />
+                    <label>Show Description</label>
+                    <textarea    
+                        id="desc" 
+                        name="desc" />
+                    <input 
+                        className="button"
+                        type="submit" 
+                        value="Submit" />
+                    <button className="button">Delete</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+}
 
 export default Login;
+
+{/* <UpcomingShowCard 
+isDesktop={isDesktop}
+img={poster}
+title="CUNY Graduate Theater"
+date="Aug 18"
+time="7:00PM"
+location="365 Fifth Ave (B/W 34th & 35th Street)"
+cost="$25"
+host="Ken Dashow"
+desc={testDesc}
+url="https://bpt.me/5763465"/> */}
