d6dfabc9970f3661047a3e7f6dfa13b7bd6f2a96
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-08-06 16:05:35 -0400

    added table to sponsor page

--- client/src/components/Sponsors.js
+++ client/src/components/Sponsors.js
@@ -16,6 +16,27 @@ function Sponsors() {
                 <Fade bottom>
                     <TextBlock title="How To Request" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
                 </Fade>
+                <Fade bottom>
+                    <div id="sp-tb">
+                        <table>
+                            <tr>
+                                <th>Option 1</th>
+                                <th>Option 2</th>
+                                <th>Option 3</th>
+                            </tr>
+                            <tr>
+                                <td>Details</td>
+                                <td>Details</td>
+                                <td>Details</td>
+                            </tr>
+                            <tr>
+                                <td>$100</td>
+                                <td>$500</td>
+                                <td>$1000</td>
+                            </tr>
+                        </table>
+                    </div>
+                </Fade>
             </div>
 
             
--- client/src/components/Submissions.js
+++ client/src/components/Submissions.js
@@ -11,9 +11,9 @@ function Submissions() {
             <div id="sub-content">
                 <h2 className="sub-header">Share your short film with audiences around the country</h2>
                 <TextBlock title="What We Bring" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-                <Fade bottom>
+                {/* <Fade bottom>
                     <TextBlock title="Prerequisites" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-                </Fade>
+                </Fade> */}
                 <Fade bottom>
                     <TextBlock title="How To Request" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
                 </Fade>
