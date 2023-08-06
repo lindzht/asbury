7fcbf62cd1dd2572c10e240784b5a97ecbb96f5f
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-08-06 17:36:44 -0400

    added show list and login styles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -22,67 +22,80 @@ else {
         <div id="login-container">
             <h1>You're Logged In!</h1>
 
-            <div id="new-show-container">
-                <form id="new-show-form">
-                    <label>Banner Image</label>
-                    <input 
-                        type="text" 
-                        id="img" 
-                        name="img"
-                        placeholder="Image" />
-                    <label>Venue</label>
-                    <input 
-                        type="text" 
-                        id="title" 
-                        name="title"
-                        placeholder="Title" />
-                    <label>Show Date</label>
-                    <input 
-                        type="date" 
-                        id="date" 
-                        name="date"
-                        placeholder="Aug 18" />
-                    <label>Show Time</label>
-                    <input 
-                        type="text" 
-                        id="time" 
-                        name="time"
-                        placeholder="7:00 PM" />
-                    <label>Address</label>
-                    <input 
-                        type="text" 
-                        id="location" 
-                        name="location"
-                        placeholder="365 Fifth Ave" />
-                    <label>Ticket Cost</label>
-                    <input 
-                        type="text" 
-                        id="cost" 
-                        name="cost"
-                        placeholder="$25" />
-                    <label>Host</label>
-                    <input 
-                        type="text" 
-                        id="host" 
-                        name="host"
-                        placeholder="First Name Last Name" />
-                    <label>Ticket URL</label>
-                    <input 
-                        type="text" 
-                        id="url" 
-                        name="url"
-                        placeholder="URL" />
-                    <label>Show Description</label>
-                    <textarea    
-                        id="desc" 
-                        name="desc" />
-                    <input 
-                        className="button"
-                        type="submit" 
-                        value="Submit" />
-                    <button className="button">Delete</button>
-                </form>
+            <div id="login-content">
+                <div id="login-l">
+                    <button>Download Mailing List</button>
+                    <h3>All Shows:</h3>
+                    <ul>
+                        <li>CUNY Graduate Theater | Aug 18 2023 <span id="edit">Edit</span><span id="delete">Delete</span></li>
+                        <li>CUNY Graduate Theater | Aug 18 2023 <span id="edit">Edit</span><span id="delete">Delete</span></li>
+                        <li>CUNY Graduate Theater | Aug 18 2023 <span id="edit">Edit</span><span id="delete">Delete</span></li>
+                    </ul>
+                </div>
+
+                <div id="new-show-container">
+                    <form id="new-show-form">
+                        <label>Banner Image</label>
+                        <input 
+                            type="text" 
+                            id="img" 
+                            name="img"
+                            placeholder="Image" />
+                        <label>Venue</label>
+                        <input 
+                            type="text" 
+                            id="title" 
+                            name="title"
+                            placeholder="Title" />
+                        <label>Show Date</label>
+                        <input 
+                            type="date" 
+                            id="date" 
+                            name="date"
+                            placeholder="Aug 18" />
+                        <label>Show Time</label>
+                        <input 
+                            type="text" 
+                            id="time" 
+                            name="time"
+                            placeholder="7:00 PM" />
+                        <label>Address</label>
+                        <input 
+                            type="text" 
+                            id="location" 
+                            name="location"
+                            placeholder="365 Fifth Ave" />
+                        <label>Ticket Cost</label>
+                        <input 
+                            type="text" 
+                            id="cost" 
+                            name="cost"
+                            placeholder="$25" />
+                        <label>Host</label>
+                        <input 
+                            type="text" 
+                            id="host" 
+                            name="host"
+                            placeholder="First Name Last Name" />
+                        <label>Ticket URL</label>
+                        <input 
+                            type="text" 
+                            id="url" 
+                            name="url"
+                            placeholder="URL" />
+                        <label>Show Description</label>
+                        <textarea    
+                            id="desc" 
+                            name="desc" />
+                        <input 
+                            className="button"
+                            type="submit" 
+                            value="Add Show" />
+                        {/* <button className="button">Delete</button> */}
+                    </form>
+                </div>
             </div>
+
         </div>
     )
 }
